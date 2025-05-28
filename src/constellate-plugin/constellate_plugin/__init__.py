b2b43ae006a75340415f901a8dbc4c62106b7915
nkelber <nkelber@gmail.com>
2025-05-28 15:41:25 -0400

    remove files and folders

--- src/constellate-plugin/constellate_plugin/__init__.py
+++
@@ -1,7 +0,0 @@
-# Jupyter Extension points
-def _jupyter_nbextension_paths():
-    return [dict(
-        section="notebook",
-        src="./static",
-        dest="constellate-plugin",
-        require="constellate-plugin/main")]
